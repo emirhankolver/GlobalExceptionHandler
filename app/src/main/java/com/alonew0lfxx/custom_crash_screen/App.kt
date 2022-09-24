ca301fd14dabb2f68a32c776b3c962e66b19d2a4
Emirhan Kolver <emirhanklvr@gmail.com>
2022-09-24 19:49:45 +0300

    Line enabled back.

--- app/src/main/java/com/alonew0lfxx/custom_crash_screen/App.kt
+++ app/src/main/java/com/alonew0lfxx/custom_crash_screen/App.kt
@@ -12,7 +12,7 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        // GlobalExceptionHandler.initialize(this,CrashActivity::class.java)
+        GlobalExceptionHandler.initialize(this,CrashActivity::class.java)
     }
 
 }
